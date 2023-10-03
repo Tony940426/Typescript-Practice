fe6e87c78cccd20846a3ac3586187f0656df0494
Tony Wang <Tony94042@gmail.com>
2023-10-03 22:13:18 +1100

    Module Done

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
 //****Generic Classes****
 //Use them if we have a lot of classes with few property changes
 //Normally inside the <> most people use T (type). Doesn't really matter what you put in there, but something short is the convention
-class ValueHolder<T> {
-    value: T;
-}
+// class ValueHolder<T> {
+//     value: T;
+// }
 
 
-//***Generic Methods****/
-const numberWrapper = (value: number): number[] => {
-    return [value]
-};
+// //***Generic Methods****/
+// const numberWrapper = (value: number): number[] => {
+//     return [value]
+// };
 
-const valueWrapper = <T>(value: T): T[] => {
-    return [value];
-}
+// const valueWrapper = <T>(value: T): T[] => {
+//     return [value];
+// }
 
-console.log(valueWrapper<number>(10));
-console.log(valueWrapper<boolean>(false))
-console.log(valueWrapper<string>('false'))
+// console.log(valueWrapper<number>(10));
+// console.log(valueWrapper<boolean>(false))
+// console.log(valueWrapper<string>('false'))
+
+// const value = valueWrapper(10)
