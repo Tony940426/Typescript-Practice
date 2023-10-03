5be38f0390903aa14b862425dc1716790ee5487b
Tony Wang <Tony94042@gmail.com>
2023-10-03 22:10:48 +1100

    Generic Functions

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
-//Generic Classes
+//****Generic Classes****
 //Use them if we have a lot of classes with few property changes
-class ValueHolder<TypeForValueProperty> {
-    value: TypeForValueProperty;
+//Normally inside the <> most people use T (type). Doesn't really matter what you put in there, but something short is the convention
+class ValueHolder<T> {
+    value: T;
 }
 
-const numberHolder = new ValueHolder<number>();
-numberHolder.value = 12
+
+//***Generic Methods****/
+const numberWrapper = (value: number): number[] => {
+    return [value]
+};
+
+const valueWrapper = <T>(value: T): T[] => {
+    return [value];
+}
+
+console.log(valueWrapper<number>(10));
+console.log(valueWrapper<boolean>(false))
+console.log(valueWrapper<string>('false'))
